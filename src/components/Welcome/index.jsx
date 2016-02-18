51d9f05c53442acce6c31dca6c8a192ececd865f
Adrian Lawrence <avjpldeveloper@aol.com>
2016-02-18 15:24:08 +0000

    Change to a stateless component

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -1,37 +1,43 @@
 import React, { View, Text } from 'react-native';
+import Button from 'react-native-button';
+import routes from '../../scripts/routes';
 
-const Welcome = () => (
-    <View style={ styles.container }>
-        <Text style={ styles.welcome }>
-            React Native Redux Starter Kit
-        </Text>
-        <Text style={ styles.instructions }>
-            Edit ./src/component/Welcome/index.jsx{'\n'}
-            to get started.
-        </Text>
-        <Text style={ styles.instructions }>
-            Press Cmd+R to reload,{'\n'}
-            Cmd+D or shake for dev menu
-        </Text>
-    </View>
+const Welcome = ({navigator}) => (
+  <View style={styles.container}>
+    <Button onPress={() => {
+          // Get a route object from the router
+          let profile = {
+            name: 'Jane',
+            photoUrl: 'https://avatars3.githubusercontent.com/u/146186?v=3&s=40',
+          };
+
+          let route = routes.getProfileRoute(profile);
+
+          // `navigator` is passed into your scene component when you have
+          // implemented getSceneClass in your route
+          navigator.push(route);
+        }}>
+      Navigate to a profile
+    </Button>
+  </View>
 );
 
 const styles = {
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 10
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 10
+  }
 };
 
 export default Welcome;
